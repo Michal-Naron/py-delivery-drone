e46ef5a6867a37babcc65f7b610d02697882f6a4
Michal-Naron <michanaron@gmail.com>
2025-04-02 12:44:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,3 +3,54 @@ class Cargo:
         self.weight = weight
 
 # write your code here
+
+class BaseRobot:
+    def __init__(self, name: str, weight: int, coords: list = [0, 0]):
+        self.name = name
+        self.weight = weight
+        self.coords = coords
+
+    def go_forward(self, step: int = 1) -> None:
+        self.coords[1] = self.coords[1] + step
+
+    def go_back(self, step: int = 1) -> None:
+        self.coords[1] = self.coords[1] - step
+
+    def go_right(self, step: int = 1) -> None:
+        self.coords[0] = self.coords[0] + step
+
+    def go_left(self, step: int = 1) -> None:
+        self.coords[0] = self.coords[0] - step
+
+    def get_info(self) -> str:
+        return f"Robot: {self.name}, Weight: {self.weight}"
+
+class FlyingRobot(BaseRobot):
+    def __init__(self, name: str, weight: int, coords: [] = [0, 0, 0]) -> \
+            None:
+        super().__init__(name, weight, coords)
+
+    def go_up(self, step: int = 1) -> None:
+        self.coords[2] = self.coords[2] + step
+
+    def go_down(self, step: int = 1) -> None:
+        self.coords[2] = self.coords[2] - step
+
+class DeliveryDrone(FlyingRobot):
+    def __init__(self, name: str, weight: int, max_load_weight: int,
+                 current_load: object = None, coords: list = [0, 0, 0]) -> \
+            None:
+        super().__init__(name, weight, coords)
+        self.max_load_weight = max_load_weight
+        self.current_load = None
+        if isinstance(current_load, Cargo):
+            self.hook_load(current_load)
+
+    def hook_load(self, cargo: object):
+            if self.current_load is None and cargo.weight <= \
+                    self.max_load_weight:
+                self.current_load = cargo
+
+    def unhook_load(self):
+            self.current_load = None
+
